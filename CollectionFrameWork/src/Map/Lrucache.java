441f8c4ca6ef6684001159b50ec2bfc503d64502
Hemant deora <hemantdeora923@gmail.com>
2026-03-22 17:05:23 +0530

    update code

--- CollectionFrameWork/src/Map/Lrucache.java
+++ CollectionFrameWork/src/Map/Lrucache.java
@@ -3,12 +3,12 @@ package Map;
 import java.util.LinkedHashMap;
 import java.util.Map.Entry;
 
-class LRUCache extends LinkedHashMap<Integer, Integer> {
+class Cache extends LinkedHashMap<Integer, Integer> {
 
     private final int capacity;
 
     // Constructor
-    public LRUCache(int capacity) {
+    public Cache(int capacity) {
         // accessOrder = true → maintains LRU order
         super(capacity, 0.75f, true);
         this.capacity = capacity;
@@ -36,17 +36,17 @@ public class Lrucache  {
         // LRU Cache (Least Recently Used) is a cache eviction policy that removes the least recently accessed items when the cache reaches its capacity. It is commonly implemented using a combination of a HashMap and a Doubly Linked List to achieve O(1) time complexity for both get and put operations.
 
 
-        LRUCache cache = new LRUCache(3);
+        Cache ca = new Cache(3);
 
-        cache.put(1, 10);
-        cache.put(2, 20);
-        cache.put(3, 30);
+        ca.put(1, 10);
+        ca.put(2, 20);
+        ca.put(3, 30);
 
-        cache.get(1); // mark as recently used
+        ca.get(1); // mark as recently used
 
-        cache.put(4, 40); // removes least recently used (key 2)
+        ca.put(4, 40); // removes least recently used (key 2)
 
-        System.out.println("Final Cache: " + cache);
+        System.out.println("Final Cache: " + ca);
 
 
     }
